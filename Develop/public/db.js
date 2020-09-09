b6bf90f2bea30fdb155d12e472a5686ede35fe41
veronica lim <veron_yan@yahoo.com.sg>
2020-09-09 18:32:29 +0800

    created request in db.js

---
+++ Develop/public/db.js
@@ -0,0 +1,26 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.online) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    console.log("Try Again " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+};
