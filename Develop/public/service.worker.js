9aa84ecfef83cd2a04c692657f2973a5db6d4f7e
veronica lim <veron_yan@yahoo.com.sg>
2020-09-10 08:58:21 +0800

    activated service worker

--- Develop/public/service.worker.js
+++ Develop/public/service.worker.js
@@ -11,10 +11,57 @@ const DATA_CACHE_NAME = 'data-cache-v8';
 
 self.addEventListener('install', evt => {
     evt.waitUntil(
-        cache.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             console.log("Files pre-cached successfully");
             return cache.addAll(FILES_TO_CACHE);
         })
     );
     self.skipWaiting();
+});
+
+// Activate service worker
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map( key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME){
+                        console.log("Remove old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.ClientRectList.claim();
+});
+
+// Fetch files
+self.addEventListener("fetch", evt => {
+    if (evt.request.url.includes("/api/")) {
+        console.log("[Service Worker] Fetch (data", evt.request.url);
+
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    returncache.match(evt.request);
+                });
+            })
+        );
+        return;
+    }
+    evt.respondWith(
+        caches.open(CACHE_NAME).then( cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request)
+            });
+        })
+    );
 });
